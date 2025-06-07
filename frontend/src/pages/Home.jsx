3a1c90e309daae48d71046d9078ede1c065b6611
sonu1680 <sonupandit1680@gmail.com>
2025-06-07 10:25:29 +0530

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import Testimonials from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
+import Preorder from '../components/Preorder'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Hero />
+      <LatestCollection />
+      <Testimonials />
+      {/* <BestSeller /> */}
+      <Preorder />
+      <Newsletter />
+      <OurPolicy />
+    </div>
+  );
+}
+
+export default Home
