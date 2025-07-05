855a098ca844892b36cc4c4f6ef191b28503da0c
sonu1680 <sonupandit1680@gmail.com>
2025-07-05 15:55:28 +0530

    backedn update render

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/api/v1/contact", contactRouter);
 
 
 app.get('/',(req,res)=>{
-    res.send("API Working")
+    res.send("API Workings")
 })
 
 app.listen(port, ()=> console.log('Server started on PORT : '+ port))
